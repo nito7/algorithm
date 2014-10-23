5c1fb4c124a83fad3974ca9099ad191dd7f797d2
nito7 <naoki@www12480uf.sakura.ne.jp>
2014-10-24 00:10:09 +0900

    add icpc problems

---
+++ problems/icpc/A.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+  while (1) {
+    int x, y, z, m;
+    cin >> x >> y >> z;
+    if (x == 0 && y == 0 && z == 0) {
+      break;
+    }
+    for (int i = 0; i < z; i++) {
+      int j = z - i; 
+      int _i = i * 100;
+      int _j = j * 100;
+
+    }
+  }
+
+  cout << 20 * (100 + 5) / 100 << endl;
+  cout << 21 * 100 / (100 + 5) << endl;
+  return 0;
+}
